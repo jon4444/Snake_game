5b56ab3475a2cbb1058d4acfd1ecddd8e6db4fd6
jon4444 <50190069+jon4444@users.noreply.github.com>
2022-09-08 11:43:40 +0100

    added food and a snake

--- play.py
+++ play.py
@@ -11,12 +11,28 @@ FOOD_COLOR='#FF0000'
 BACKGROUND_COLOR='#000000'
 
 class Snake:
-    pass
+    def __init__(self):
+        self.body_size=BODY_PARTS
+        self.coordinates=[]
+        self.squares=[]
+
+        for i in range(0, BODY_PARTS):
+            self.coordinates.append([0, 0])
+
+        for x, y, in self.coordinates:
+            square = canvas.create_rectangle(x, y, x+SPACE_SIZE, y+SPACE_SIZE, fill=SNAKE_COLOR, tag="snake")
+            self.squares.append(square)
 
 class Food:
-    pass
+    def __init__(self):
+        x=random.randint(0, (GAME_WIDTH/SPACE_SIZE)-1)*SPACE_SIZE
+        y=random.randint(0, (GAME_HEIGHT/SPACE_SIZE)-1)*SPACE_SIZE
 
-def next_turn():
+        self.coordinates=[x,y]
+
+        canvas.create_oval(x, y, x+SPACE_SIZE, y+SPACE_SIZE, fill=FOOD_COLOR, tag="FOOD")
+
+def next_turn(snake, turn):
     pass
 
 def change_direction(new_direction):
@@ -41,7 +57,20 @@ label.pack()
 canvas = Canvas(window, bg=BACKGROUND_COLOR, height=GAME_HEIGHT, width=GAME_WIDTH)
 canvas.pack()
 
+window.update()
 
+window_width = window.winfo_width()
+window_height = window.winfo_height()
+screen_width = window.winfo_screenwidth()
+screen_height = window.winfo_screenheight()
 
+x=int((screen_width/2) - (window_width/2))
+y=int((screen_height/2) - (window_height/2))
+
+window.geometry(f"{window_width}x{window_height}+{x}+{y}")
+
+#snake object and food object
+snake = Snake()
+food =Food()
 
 window.mainloop()
